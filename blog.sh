00a0b7796e38803f41b1a630f2b3dc5b6eaef4d5
Krishna Komarshetty <krishna@adsparx.in>
2016-07-29 17:49:19 +0530

    blog sqlite db and blog.sh with post commands

--- blog.sh
+++ blog.sh
@@ -3,8 +3,49 @@
 #switch cases to handle post category and help
 case $1 in
     post)
+	case $2 in
+		add)
+			if ([ ! -z "$3" -a "$3" != " " ] && [ ! -z "$3" -a "$4" != " " ])
+			then
+				sqlite3 blog "insert into post(tilte,content) values('$3','$4');"				
+			fi		
+		;;
+		list)
+			sqlite3 blog "select * from post;"
+		;;
+		search)
+			if [ ! -z "$3" -a "$3" != " " ]
+                        then
+                                sqlite3 blog "select * from post where tilte = '$3';"
+                        fi
+	
+		;;
+		*)
+			echo "Usage: $0 --help"
+        		exit 3
+        	;;	
+	esac
+
+	:
    ;;
     category)
+	case $2 in
+		add)
+
+                ;;
+                list)
+
+                ;;
+                assign)
+
+                ;;
+                *)
+                        echo "Usage: $0 --help"
+                        exit 3
+                ;;
+        esac
+
+        :
    ;;
    --help)
    	echo "Usage: $0 [POST|CATEGORY]... [OPTIONS]..."
